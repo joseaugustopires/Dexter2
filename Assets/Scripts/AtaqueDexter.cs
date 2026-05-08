2b303f25ab5f5d319d15a95e7f033afd256d3b6d
joseaugustopires <joseaugustopiresifrj@gmail.com>
2026-05-08 19:45:01 -0300

    .

--- Assets/Scripts/AtaqueDexter.cs
+++ Assets/Scripts/AtaqueDexter.cs
@@ -6,11 +6,24 @@ public class AtaqueDexter : MonoBehaviour
     public Collider2D hitboxAtaque;
     public float tempoHitboxAtiva = 0.15f;
     public float tempoEntreAtaques = 0.3f;
+    public float distanciaHitbox = 0.32f;
 
     private bool podeAtacar = true;
+    private bool olhandoDireita = true;
 
     void Update()
     {
+        float movimentoHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (movimentoHorizontal > 0)
+        {
+            olhandoDireita = true;
+        }
+        else if (movimentoHorizontal < 0)
+        {
+            olhandoDireita = false;
+        }
+
         if (Input.GetMouseButtonDown(0) && podeAtacar)
         {
             StartCoroutine(Atacar());
@@ -20,6 +33,19 @@ public class AtaqueDexter : MonoBehaviour
     IEnumerator Atacar()
     {
         podeAtacar = false;
+
+        Vector3 posicaoHitbox = hitboxAtaque.transform.localPosition;
+
+        if (olhandoDireita)
+        {
+            posicaoHitbox.x = distanciaHitbox;
+        }
+        else
+        {
+            posicaoHitbox.x = -distanciaHitbox;
+        }
+
+        hitboxAtaque.transform.localPosition = posicaoHitbox;
 
         hitboxAtaque.enabled = true;
 
